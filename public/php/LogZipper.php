edb73864d8a0e089ac1cda06887f9c1e5fc06824
Martin Schulze Beckendorf <matt.sb@hotmail.de>
2018-08-06 10:10:22 +0200

    Change: Adjusted LogZipper filesize Logging

--- public/php/LogZipper.php
+++ public/php/LogZipper.php
@@ -84,6 +84,6 @@
   function humanFilesize($bytes, $decimals = 2) {
     $sz = 'BKMGTP';
     $factor = floor((strlen($bytes) - 1) / 3);
-    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor] . "B";
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . " " . @$sz[$factor] . "B";
   }
  ?>
