b43f421781b84ce6ccd8c59d646b57ddc5cdd4c0
Kiddnils <nils.ueter@gmail.com>
2018-10-14 20:31:44 +0200

    fixed error on small viewports

--- src/Smartphone.js
+++ src/Smartphone.js
@@ -24,10 +24,6 @@ class Smartphone extends Component {
                 height:
                   "calc(calc(100vh - 510px) * " +
                   this.props.smartphone.length / 165 +
-                  ")",
-                paddingTop:
-                  "calc(calc(100vh - 510px) * " +
-                  (1 - this.props.smartphone.length / 165) +
                   ")"
               }}
               className="qtip-img"
