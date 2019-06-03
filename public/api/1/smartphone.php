1f013648a2eb0b75e9bbd9565a50f3d8035e6bee
Martin Schulze Beckendorf <matt.sb@hotmail.de>
2019-06-03 09:52:59 +0200

    /smartphone endpoint draft

---
+++ public/api/1/smartphone.php
@@ -0,0 +1,13 @@
+<?php
+    //headers
+    header('Access-Control-Allow-Methods: GET');
+    header('Content-Type: application/json');
+    // get the HTTP method of the request
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method !== 'GET') {
+        header('HTTP/1.1 405 Method not allowed Error');
+        die();
+    }
+
+    //Retrieve Smartphone data, generate json and return
+?>
