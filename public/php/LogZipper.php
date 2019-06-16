e68b4ae5387264a506e2d4716f804a724849b0e2
Martin Schulze Beckendorf <matt.sb@hotmail.de>
2019-06-16 14:37:12 +0200

    Logzipping: Include recent files bigger 20MB

--- public/php/LogZipper.php
+++ public/php/LogZipper.php
@@ -14,7 +14,6 @@
   require_once "Logger.php";
 
   logToFile("LogZipper", "Starting to zip Logs of last Week");
-  echo "br>Starting to zip Logs of last Week<br>";
 
   //Create archive directory if not existing
   if (!file_exists('../logs/archive')) {
@@ -29,24 +28,21 @@
   $date_current = new DateTime();
   $date_current->setTimestamp(mktime(0,0,0));
   logToFile("LogZipper", "Current date start time " . $date_current->format('d.m.Y H:i:s'));
-  echo "<br>Current date start time" . $date_current->format('d.m.Y H:i:s') . "<br>";
   logToFile("LogZipper", "Current date timestamp " . $date_current->getTimestamp());
-  echo "Current date timestamp " . $date_current->getTimestamp() . "<br>";
 
   $date_before = new DateTime();
   $date_before->setTimestamp(mktime(0,0,0));
   $date_before->modify('-7 day');
   logToFile("LogZipper", "Startdate start time of last week: " . $date_before->format('d.m.Y H:i:s'));
-  echo "<br>Startdate start time of last week: " . $date_before->format('d.m.Y H:i:s') . "<br>";
   logToFile("LogZipper", "Startdate timestamp: " . $date_before->getTimestamp());
-  echo "Startdate timestamp: " . $date_before->getTimestamp() . "<br>";
 
   $fileSizeSum = 0;
 
   $dir = new DirectoryIterator('../logs');
   $logfiles = [];
   foreach ($dir as $fileinfo) {
-    if (!$fileinfo->isDot() AND !$fileinfo->isDir() AND $fileinfo->getMTime() >= $date_before->getTimestamp() AND $fileinfo->getMTime() < $date_current->getTimestamp()) {
+    //Dateien der letzten Woche oder größer als 20 Megabyte auswählen
+    if (!$fileinfo->isDot() AND !$fileinfo->isDir() AND (($fileinfo->getMTime() >= $date_before->getTimestamp() AND $fileinfo->getMTime() < $date_current->getTimestamp()) OR $fileinfo->getSize() > 20000000)) {
         $zip->addFile($fileinfo->getPathName(), $fileinfo->getFileName());
         $fileSizeSum = $fileSizeSum + filesize($fileinfo->getPathName());
         //Solange die zip nocht nicht geclosed wurde sind die Dateien die in die zip sollen gelocked
@@ -56,24 +52,17 @@
   }
 
   logToFile("LogZipper", "Number of logfiles: " . $zip->numFiles);
-  echo "<br>Number of logfiles: " . $zip->numFiles . "<br>";
   logToFile("LogZipper", "File size Sum: " . humanFilesize($fileSizeSum));
-  echo "<br>File size Sum: " . humanFilesize($fileSizeSum) . "<br>";
   logToFile("LogZipper", "Status of the new zip file:" . $zip->status);
-  echo "Status of the new zip file:" . $zip->status . "<br>";
 
   $ret = $zip->close();
   logToFile("LogZipper", "Closed zipfile with result: " . ($ret ? "true" : "false"));
-  echo "Closed zipfile with result: " . ($ret ? "true" : "false") . "<br>";
   logToFile("LogZipper", "Zip filesize: " . humanFilesize(filesize($filename)));
-  echo "Zip filesize: " . humanFilesize(filesize($filename)) . "<br>";
 
   logToFile("LogZipper", "Logfiles");
-  echo "<br>Logfiles<br>";
   asort($logfiles);
   foreach ($logfiles as $i => $logfile) {
     logToFile("LogZipper", $logfile);
-    echo $logfile . "<br>";
     unlink('../logs/' . $logfile);
   }
 
