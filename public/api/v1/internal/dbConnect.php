711e21c8485fbafbe640498ba2fad409b508930c
Martin Schulze Beckendorf <matt.sb@hotmail.de>
2019-07-15 18:06:09 +0200

    Grammar fixes

--- public/api/v1/internal/dbConnect.php
+++ public/api/v1/internal/dbConnect.php
@@ -5,12 +5,12 @@
 
         // Try and connect to the database, if a connection has not been established yet
         if(!isset($connection)) {
-            // Load configuration as an array. Use the actual location of your configuration file
+            // Load configuration as an array
             $config = parse_ini_file('config.ini'); 
             $connection = mysqli_connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
         }
 
-    // If connection was not successful, handle the error
+        // If connection was not successful, handle the error
         if($connection === false) {
             // Handle error - notify administrator, log to a file, show an error screen, etc.
             return mysqli_connect_error(); 
